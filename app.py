ae350e4026111706be66a0a5328613dbfd4f53e9
satinder <satindersingh472@gmail.com>
2022-10-11 16:43:48 -0600

    finished two endpoints

--- app.py
+++ app.py
@@ -6,4 +6,21 @@ import json
 
 app = Flask(__name__)
 
+@app.post('/api/hero')
+def add_hero():
+    invalid = verify_endpoints_info(request.json, ['name','description','image_url'])
+    if(invalid != None):
+        return make_response(json.dumps(invalid , default=str),400)
+    results_json = get_display_results('call add_hero(?,?,?)',[request.json.get('name'),request.json.get('description'),request.json.get('image_url')])
+    return results_json
+
+@app.post('/api/villian')
+def add_villian():
+    invalid = verify_endpoints_info(request.json,['name','description','image_url','hero_id'])
+    if(invalid != None):
+        return make_response(json.dumps(invalid, default=str),400)
+    results_json = get_display_results('call add_villian(?,?,?,?)',
+    [request.json.get('name'),request.json.get('description'),request.json.get('image_url'),request.json.get('hero_id')])
+    return results_json
+
 app.run(debug=True)
