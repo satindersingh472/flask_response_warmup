395f26f4dee032328064d32b61f39d0a65349122
satinder <satindersingh472@gmail.com>
2022-10-11 16:55:48 -0600

    finished

--- app.py
+++ app.py
@@ -23,4 +23,18 @@ def add_villian():
     [request.json.get('name'),request.json.get('description'),request.json.get('image_url'),request.json.get('hero_id')])
     return results_json
 
+
+@app.get('/api/villian')
+def all_villians_for_a_hero():
+    invalid = verify_endpoints_info(request.args, ['hero_id'])
+    if(invalid != None):
+        return make_response(json.dumps(invalid,default=str),400)
+    results_json = get_display_results('call all_villians_for_a_hero(?)',[request.args.get('hero_id')])
+    return results_json
+
+@app.get('/api/hero')
+def all_heros():
+    results_json = get_display_results('call all_heros()',[])
+    return results_json
+
 app.run(debug=True)
